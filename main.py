07c6ec263cc64445b859ff0c1a1e9194e521b270
Ace7260 <irano7260@gmail.com>
2023-05-09 02:38:10 +0200

    display of the content of element number 5

--- main.py
+++ main.py
@@ -84,3 +84,8 @@ if __name__ == '__main__':
     print("\n1) Affichage du nombre de fois que valeur 3 apparait dans la tuple")
     print("--------------------------------------------------")
     print(chiffre.count(3))
+
+    #2) Affichage du contenu de l'element numero 5
+    print("\n2) Affichage du contenu de l'element numero 5")
+    print("--------------------------------------------------")
+    print(chiffre[4])
