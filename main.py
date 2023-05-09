1f115500ac2be0178b88a8128a0c6c1ca8f7334d
Ace7260 <irano7260@gmail.com>
2023-05-09 02:13:29 +0200

    update element number 5

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ if __name__ == '__main__':
     # Question I
     # Creation d'une liste de 10 elements de type chaîne de caractères
     animaux = ["chat", "chien", "souris", "oiseau", "poisson", "cheval", "vache", "mouton", "cochon", "lapin"]
+    
     # 1- Affichage des elements de la liste
     print("1) Affichage des elements de la liste")
     print("--------------------------------------------------")
     print(animaux)
     print("--------------------------------------------------\n")
+    
+    # 2- Changer le contenu de l'element numero 5
+    print("2) Changement du contenu de l'element numero 5")
+    print("--------------------------------------------------")
+    animaux[4] = "requin"
+    print(animaux)
+
