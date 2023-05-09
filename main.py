cb3d7003d457ad53ed88cb8f6c1a3141d6430754
Ace7260 <irano7260@gmail.com>
2023-05-09 02:27:35 +0200

    deleting list

--- main.py
+++ main.py
@@ -66,3 +66,7 @@ if __name__ == '__main__':
     print("--------------------------------------------------")
     animaux.clear()
     print(animaux)
+    
+    # #11 Suppression de la liste
+    del animaux
+
