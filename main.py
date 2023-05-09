629d7f3cb90530f5e9d562f2c299ced9ba5bc8fa
Ace7260 <irano7260@gmail.com>
2023-05-09 02:08:07 +0200

    display list

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ if __name__ == '__main__':
     # Question I
     # Creation d'une liste de 10 elements de type chaîne de caractères
     animaux = ["chat", "chien", "souris", "oiseau", "poisson", "cheval", "vache", "mouton", "cochon", "lapin"]
+    # 1- Affichage des elements de la liste
+    print("1) Affichage des elements de la liste")
+    print("--------------------------------------------------")
+    print(animaux)
+    print("--------------------------------------------------\n")
