06b8218ad17e493f622d5f0448125b539eb5f2b7
Ace7260 <irano7260@gmail.com>
2023-05-09 02:06:51 +0200

    creation of list

---
+++ main.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    # Question I
+    # Creation d'une liste de 10 elements de type chaîne de caractères
+    animaux = ["chat", "chien", "souris", "oiseau", "poisson", "cheval", "vache", "mouton", "cochon", "lapin"]
